f1e0a73d661de66645b26640911ffb4fa8e1596b
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-09-09 23:11:09 +0200

    initial commit for our quiz application(programmatic autolayout prcatice

---
+++ QUIZOEY/Controller/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  QUIZOEY
+//
+//  Created by El nino Cholo on 2020/09/09.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController : UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
